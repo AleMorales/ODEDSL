b6b17ac083921366f4d581016e86df4c5c3a0243
Alejandro Morales <morales.s.alejandro@gmail.com>
2015-02-10 17:07:21 +0100

    Extensive changes to ensure the tokenizer is capable of creating the logical lines. Export is no longer a keyword but rather the keywords that can be exported have been duplicated with the prefix "export_". Also, compartments are no longer processed with the expression "as parameter/state/constant". Rather, compartments are no "constant_compartment" or "variable_compartment".

--- src/parser.jl
+++ src/parser.jl
@@ -10,15 +10,30 @@ end
 
 # Look a pattern consisting of a `#` followed by 0 or more character of any type except newline
 # and ending in newline
-strip_comments(source::String) = replace(source, r"\#.*\n","")
+strip_comments(source::String) = replace(source, r"\#.*\n"," ")
 
 # Keywords that id the type of sentence
 const start_keywords = ["mereaction", "reaction", "equation", "state", "species",
-                  "component", "parameter", "constant", "forcing", "compartment",
-                  "meequation"]
-
-# Substitute \n and \t
-strip_tabs_newline(source::String) = replace(source, r"\n|\t", "")
+                        "component", "parameter", "constant", "forcing", "constant_compartment",
+                        "meequation","export_equation","export_reaction","variable_compartment",
+                        "export_meequation","export_mereaction"]
 
 # Tokenize the string by whitespace
-tokenize(source::String) = split(source, r"\s+", keep = false)
+tokenize(source::String) = split(source, r"\s|\n|\t", keep = true)
+
+# Separate into logical lines
+function logical_lines(source::Array{SubString{ASCIIString},1})
+  lines = ASCIIString[]
+  c = 0
+  for i in source
+    if i == ""
+      continue
+    elseif in(i, start_keywords)
+      c += 1
+      push!(lines, i)
+    else
+      lines[c] *= " $i "
+    end
+  end
+  return lines
+end
