90e03a0f8edd20be4461bdcd39780159062ec51c
AleMorales <morales.s.alejandro@gmail.com>
2015-02-20 15:58:55 +0100

    Add types to store the information about the model.

--- src/parser.jl
+++ src/parser.jl
@@ -1,4 +1,6 @@
 
+using .DataTypes
+
 # `read_file` will read the entire file as a single string. From this string we can
 # start parsing the model description
 function read_file(filename::String)
